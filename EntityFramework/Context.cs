595ade28e8eec6d65ab1fde10f4ede45056d7a49
Atish Krishen Hariparsad <21825470@dut4life.ac.za>
2021-06-07 15:24:15 +0200

    Add project files.

---
+++ EntityFramework/Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework
+{
+    class Context : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+
+        private string DatabasePath { get; set; }
+
+        public Context()
+        {
+
+        }
+
+        public Context(string databasePath)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={DatabasePath}");
+        }
+
+    }
+}
---
+++ EntityFramework/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string StudentEmail { get; set; }
+
+        public string StudentPassword { get; set; }
+
+        public string StudentPhone { get; set; }
+
+        public string StudentAddress { get; set; }
+
+    }
+}
